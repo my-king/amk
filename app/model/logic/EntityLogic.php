02a122f1000be3e2b63a034c9dc3e719d50be52f
Igor da Hora <igordahora@gmail.com>
2015-02-18 17:58:11 -0300

    Corrigido bug do nome do objeto
    Agora a aplicação força a primeira letra do objeto a ser maiuscula

--- app/model/logic/EntityLogic.php
+++ app/model/logic/EntityLogic.php
@@ -3,14 +3,14 @@
 class EntityLogic {
 
     public function atualizar($solicitante, $params) {
-
         if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $objeto = ucfirst($_POST['objeto']);
             $entity = TObjetoHtmlHelper::mountEstruturaClassHtml($_POST);
             $path_entity = SecurityHelper::getInstancia()->getSistema()->getEntity();
-            ExportFileHelper::criarArquivo($path_entity . $_POST['objeto'] . ".php", $entity);
+            ExportFileHelper::criarArquivo($path_entity . $objeto . ".php", $entity);
             unset($entity, $path_entity);
 
-            TFeedbackHelper::notifySuccess("A Entity [ {$_POST['objeto']} ] foi atualizada com sucesso!!!");
+            TFeedbackHelper::notifySuccess("A Entity [ {$objeto} ] foi atualizada com sucesso!!!");
             RedirectorHelper::addUrlParameter('file', $params['file']);
             RedirectorHelper::goToControllerAction($solicitante->modulo, $solicitante->page);
         } else {
@@ -21,30 +21,30 @@ class EntityLogic {
 
     public function cadastrar($solicitante, $params) {
         if ($_SERVER['REQUEST_METHOD'] === "POST") {
-
+            $objeto = ucfirst($_POST['objeto']);
             $entity = TObjetoHtmlHelper::mountEstruturaClassHtml($_POST);
             $path_entity = SecurityHelper::getInstancia()->getSistema()->getEntity();
-            ExportFileHelper::criarArquivo($path_entity . $_POST['objeto'] . ".php", $entity);
-            TFeedbackHelper::notifySuccess("A Entity [ {$_POST['objeto']} ] foi cadastrada com sucesso!!!");
+            ExportFileHelper::criarArquivo($path_entity . $objeto . ".php", $entity);
+            TFeedbackHelper::notifySuccess("A Entity [ {$objeto} ] foi cadastrada com sucesso!!!");
             unset($entity, $path_entity);
 
             if (isset($_POST['dao'])) {
                 $path_Dao = SecurityHelper::getInstancia()->getSistema()->getDao();
-                $Dao = TObjetoHtmlHelper::mountClassDao($_POST['objeto']);
-                ExportFileHelper::criarArquivo($path_Dao . $_POST['objeto'] . "DAO.php", $Dao);
-                TFeedbackHelper::notifySuccess("A Dao [ {$_POST['objeto']}DAO ] foi cadastrada com sucesso!!!");
+                $Dao = TObjetoHtmlHelper::mountClassDao($objeto);
+                ExportFileHelper::criarArquivo($path_Dao . $objeto . "DAO.php", $Dao);
+                TFeedbackHelper::notifySuccess("A Dao [ {$objeto}DAO ] foi cadastrada com sucesso!!!");
                 unset($path_Dao, $Dao);
             }
 
             if (isset($_POST['logic'])) {
                 $path_Logic = SecurityHelper::getInstancia()->getSistema()->getLogic();
-                $Logic = TObjetoHtmlHelper::mountClassLogic($_POST['objeto']);
-                ExportFileHelper::criarArquivo($path_Logic . $_POST['objeto'] . "Logic.php", $Logic);
-                TFeedbackHelper::notifySuccess("A Logic [ {$_POST['objeto']}Logic ] foi cadastrada com sucesso!!!");
+                $Logic = TObjetoHtmlHelper::mountClassLogic($objeto);
+                ExportFileHelper::criarArquivo($path_Logic . $objeto . "Logic.php", $Logic);
+                TFeedbackHelper::notifySuccess("A Logic [ {$objeto}Logic ] foi cadastrada com sucesso!!!");
                 unset($Logic, $path_Logic);
             }
 
-            RedirectorHelper::addUrlParameter('file', $_POST['objeto']);
+            RedirectorHelper::addUrlParameter('file', $objeto);
             RedirectorHelper::goToControllerAction($solicitante->modulo, 'editar');
         } else {
             TFeedbackHelper::notifyWarning('Ocorreu um error na requisição, acesse corretamente.');
